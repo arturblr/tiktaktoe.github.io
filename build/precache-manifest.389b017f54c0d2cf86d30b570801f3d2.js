572ecbabe4eb49fd40649de405047b91a425c32a
Artur Betsin <62673796+arturblr@users.noreply.github.com>
2020-04-11 16:18:53 +0300

    1st upload

---
+++ build/precache-manifest.389b017f54c0d2cf86d30b570801f3d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aedb0218486e3532f9678ebc1114c288",
+    "url": "/tiktaktoe.github.io/index.html"
+  },
+  {
+    "revision": "91c09bc41a60878b825e",
+    "url": "/tiktaktoe.github.io/static/css/main.d2e323a0.chunk.css"
+  },
+  {
+    "revision": "7599bf2db974bd532148",
+    "url": "/tiktaktoe.github.io/static/js/2.1a0425fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tiktaktoe.github.io/static/js/2.1a0425fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c09bc41a60878b825e",
+    "url": "/tiktaktoe.github.io/static/js/main.d69feba5.chunk.js"
+  },
+  {
+    "revision": "19dd9c827a0f65a120e7",
+    "url": "/tiktaktoe.github.io/static/js/runtime-main.88f0e086.js"
+  }
+]);
